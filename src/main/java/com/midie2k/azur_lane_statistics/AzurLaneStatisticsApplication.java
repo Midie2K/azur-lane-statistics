6dc0c5609698816fca834eef04cadcf4cc329c2c
Midie <midie2k.games@gmail.com>
2025-05-21 20:13:25 +0200

    #1 init project

---
+++ src/main/java/com/midie2k/azur_lane_statistics/AzurLaneStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.midie2k.azur_lane_statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurLaneStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurLaneStatisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midie2k/azur_lane_statistics/AzurLaneStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midie2k.azur_lane_statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurLaneStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
